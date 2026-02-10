939879098487370d77e7dcb227d4b5b456708d38
Erion-Pllana <erionpllana35@gmail.com>
2026-02-10 17:48:04 +0100

    .

---
+++ lists.py
@@ -0,0 +1,15 @@
+to_do_list = ["MATH" , "BIO", "GEO"]
+
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+
+print(first_item)
+print(second_item)
+print(third_item)
+
+shopping_list = ["apple" , "banana" , 4 , 4.5]
+
+shopping_list.append("oranges")
+
+print(shopping_list)
---
+++ variables.py
@@ -0,0 +1,16 @@
+temperature = 42.5
+name = 'Erion'
+
+print(type(temperature))
+print(type(name))
+
+age = 17
+print("age is:", age)
+
+age=age+1
+print("now this is my", age)
+
+first_name = 'Eriom'
+last_name = 'Pllana'
+full_name=first_name + " " + last_name
+print(full_name)
